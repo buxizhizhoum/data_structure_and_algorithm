2dedc2c214a2861058785e7fbbb98f4c8957f247
buxizhizhoum <mapeaks@126.com>
2018-12-11 20:01:37 +0800

    Add dense_graph.py

---
+++ graph/dense_graph.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+graph implement with adjacent matrix
+
+it is better to implement graph with adjacent matrix when the graph is dense
+"""
+from __future__ import print_function
+
+
+class DenseGraph(object):
+    def __init__(self, n, directed=False):
+        self.n = n  # vertex number
+        self.m = 0  # edge number
+        self.directed = directed
+        self.matrix = [[0 for i in range(n)] for j in range(n)]
+
+    def add_edge(self, i, j):
+        if 0 <= i < self.n and 0 <= j < self.n:
+            if self.has_edge(i, j):
+                # if there is already edge, return
+                return
+            # add edge only when there is no edge
+            self.matrix[i][j] = 1
+            if self.directed is False:
+                self.matrix[j][i] = 1
+            self.m += 1  # update edge number
+        else:
+            raise ValueError("The given two nodes is illegal")
+
+    def has_edge(self, i, j):
+        if 0 <= i < self.n and 0 <= j < self.n:
+            return self.matrix[i][j]
+        raise ValueError("The given two nodes is illegal")
+
+    def edge_count(self):
+        return self.m
+
+    def vertex_count(self):
+        return self.n
+
+    def show(self):
+        for i in range(self.n):
+            for j in range(self.n):
+                print(self.matrix[i][j], end=" ")
+            print("\n")
+
+
+if __name__ == "__main__":
+    import random
+
+    graph = DenseGraph(10)
+    for _ in range(10):
+        i = random.randrange(0, 10)
+        j = random.randrange(0, 10)
+
+        graph.add_edge(i, j)
+
+    graph.show()
+
+
+
+
+
+
+
+
+
+
+
+
