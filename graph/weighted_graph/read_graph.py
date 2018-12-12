0a2a19d08d69449c723301c2fdccf843848d69bf
buxizhizhoum <mapeaks@126.com>
2018-12-12 11:20:54 +0800

    Add read_graph.py

---
+++ graph/weighted_graph/read_graph.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from sparse_graph import SparseGraph as Graph
+
+
+def read_graph(fullname):
+    with open(fullname, "r") as f:
+        lines = f.readlines()
+
+    n, m = lines[0].split()
+    graph = Graph(int(n))
+    for line in lines[1:]:
+        node_from, node_to, weight = line.split()
+
+        graph.add_edge(int(node_from), int(node_to), float(weight))
+
+    graph.show()
+
+
+if __name__ == "__main__":
+    filename = "/home/buxizhizhoum/1-Work/2-Codes/algorithm/graph/weighted_graph/test_graph.txt"
+    read_graph(filename)
