3f57bbf1b8f42d64c3d42753075490993323512c
buxizhizhoum <mapeaks@126.com>
2018-12-25 11:44:32 +0800

    more understanding on binary search

--- recursion/binary_search.py
+++ recursion/binary_search.py
@@ -26,16 +26,54 @@ def search(data, target, start=None, end=None):
     if data[mid] == target:
         return mid
     if data[mid] > target:
-        end = mid
+        end = mid - 1
     elif data[mid] < target:
-        start = mid
+        start = mid + 1
 
     return search(data, target, start, end)
+
+
+def search_1(data, target):
+    """
+    binary search
+    :param data: list to search
+    :param target: which number to find
+    :return: index of number if founded else None
+    """
+    if not data:
+        return None
+
+    start = 0
+    end = len(data) - 1
+
+    return _search(data, target, start, end)
+
+
+def _search(data, target, start, end):
+    """
+    recursion binary search
+    :param data:
+    :param target:
+    :param start:
+    :param end:
+    :return:
+    """
+    if start > end:
+        return
+    mid = (start + end) // 2
+    if data[mid] == target:
+        return mid
+    if data[mid] > target:
+        end = mid - 1
+    elif data[mid] < target:
+        start = mid + 1
+    return _search(data, target, start, end)
 
 
 if __name__ == "__main__":
     test_data = range(100)
     print(search(test_data, 80))
+    print(search_1(test_data, 80))
 
 
 
--- sort_and_search/binary_search.py
+++ sort_and_search/binary_search.py
@@ -21,7 +21,44 @@ def binary_search(alist, item):
     return start
 
 
+def binary_search_1(nums, target):
+    l = 0
+    r = len(nums) - 1  # [l, r]
+
+    while l <= r:
+        mid = l + (r - l) // 2
+        if nums[mid] == target:
+            return mid
+
+        elif nums[mid] < target:
+            l = mid + 1
+        else:  # nums[mid] > target
+            r = mid - 1
+
+    return False
+
+
+def binary_search_2(nums, target):
+    # todo: compare the boundary condition with function before
+    l = 0
+    r = len(nums)   # [l, r)
+
+    while l < r:
+        mid = l + (r - l) // 2
+        if nums[mid] == target:
+            return mid
+
+        elif nums[mid] < target:
+            l = mid + 1
+        else:  # nums[mid] > target
+            r = mid
+
+    return False
+
+
 if __name__ == "__main__":
     test_list = range(10)
     item_to_find = 6
     print binary_search(test_list, item_to_find)
+    print binary_search_1(test_list, item_to_find)
+    print binary_search_2(test_list, item_to_find)
